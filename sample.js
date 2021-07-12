83eb86dbb05c01cc9bb5d87f7db786ccb00f02fa
Hiten Soni <soni_hiten@yahoo.com>
2021-07-12 10:02:24 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww22540\viewh18080\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 [\
+  \{\
+    "name": "Aerial View",\
+    "author": "Felfin Evin",\
+    "url": "https://source.unsplash.com/XvFfPTSP2gA/1400x0",\
+    "thumbUrl": "https://source.unsplash.com/XvFfPTSP2gA/200x0"\
+  \}\
+  ]\
+}
